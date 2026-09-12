beb3c647a39fa2d0ecfc50003fd7e302698e8828
Abhishek Manjhi <90549183+Abhishekknown@users.noreply.github.com>
2026-09-12 21:10:35 +0530

    Avoid connecting Tag Manager from wrong account

--- cookie-consent.js
+++ cookie-consent.js
@@ -1,7 +1,6 @@
 (() => {
   const key = 'uplof-cookie-choice';
   const measurementId = 'G-36B8PX5BFP';
-  const tagManagerId = 'GTM-MR3F73PQ';
 
   const loadAnalytics = () => {
     if (window.__uplofAnalyticsLoaded || !measurementId) return;
@@ -20,15 +19,6 @@
     script.async = true;
     script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
     document.head.append(script);
-
-    if (tagManagerId && !window.__uplofTagManagerLoaded) {
-      window.__uplofTagManagerLoaded = true;
-      window.dataLayer.push({ 'gtm.start': Date.now(), event: 'gtm.js' });
-      const gtm = document.createElement('script');
-      gtm.async = true;
-      gtm.src = `https://www.googletagmanager.com/gtm.js?id=${tagManagerId}`;
-      document.head.append(gtm);
-    }
 
     document.addEventListener('submit', () => {
       window.dataLayer.push({ event: 'generate_lead', lead_source: 'website_form' });
